9647f48c7a1f3aea0599a0b95deb3f442786473c
Venci <venci34@gmail.com>
2022-01-04 19:40:52 +0200

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,17 @@
 import "../scss/app.scss";
+import "regenerator-runtime/runtime";
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
+  const pokemonList = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
+    .then((response) => response.json())
+    .then((res) => res.results);
+  pokemonList.forEach((pokemon) => {
+    const eachPokemon = document.createElement("li");
+    eachPokemon.innerText = pokemon.name;
+    ul.appendChild(eachPokemon);
+  });
+  document.body.appendChild(ul);
 });
